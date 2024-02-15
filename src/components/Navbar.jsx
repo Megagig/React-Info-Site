d279da037856e5f2146b9d8bb73bfad27324f6a4
megagig <megagigsoftwaresolutions@gmail.comm>
2024-02-15 11:24:14 +0100

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="nav-menu">
+      <img src="../Images/reactjs-icon.png" alt="React Logo" />
+      <h3>ReactFacts</h3>
+      <h4>React Course - Project 1</h4>
+    </nav>
+  );
+};
+
+export default Navbar;
