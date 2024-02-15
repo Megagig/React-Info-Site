df17bd972fa910b2d1934aae6b5ef8792c925627
megagig <megagigsoftwaresolutions@gmail.comm>
2024-02-15 12:21:02 +0100

    finish the styling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,7 @@ const Main = () => {
   return (
     <main>
       <h1 className="main-text">Fun facts about React</h1>
-      <ul>
+      <ul className="main-lists">
         <li>Was first released in 2013</li>
         <li>Was originally created by Jordan Walke</li>
         <li>Has well over 100K stars on GitHub</li>
